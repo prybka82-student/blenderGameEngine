b447c5e96daae738ca672498b7efca739836f752
Piotr Rybka <piotr.rybka82@gmail.com>
2021-02-05 22:01:07 +0100

    initial commit

---
+++ Unity/BlenderGameObject/Assets/shipController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipController : MonoBehaviour
+{
+    bool _move = false;
+
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+            _move = !_move;
+
+        if (_move)
+            transform.GetComponent<Rigidbody>().velocity = new Vector3(Speed, 0, 0);
+    }
+}
